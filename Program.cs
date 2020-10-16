deaad827c44625ccf0e10e599343306c569a0bda
Will <gywjoy@gmail.com>
2020-10-15 18:07:11 -0600

    add Unit2Quiz2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            int sum = Unit2Quiz2.SumBetween(7, 12);
+            Console.WriteLine("Sum is: " + sum);
+            string myStr = "Hello World";
+            myStr = Unit2Quiz2.RemoveVowels(myStr);
+            Console.WriteLine("myStr without vowels: " + myStr);
+            Unit2Quiz2.Func();
+
         }
     }
 }
---
+++ Unit2Quiz2.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstProject
+{
+    public class Unit2Quiz2
+    {
+        public static int SumBetween(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+
+            }
+            return sum;
+        }
+
+        public static string RemoveVowels(string str)
+        {
+            string result = "";
+            string vowels = "aeiou";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (vowels.IndexOf(str[i]) == -1)
+                {
+                    result += str[i];
+
+                }
+            }
+            return result;
+        }
+
+        public static void Func()
+        {
+            int a = 1;
+            FuncA(a);
+            int b = 2;
+            b = FuncB(b);
+            var c = new List<int>() { 8, -9, 12 };
+            c.Add(15);
+            Funcc(c);
+            var d = new List<int>() { -5, 6, 4 };
+            FuncD(d);
+            var e = new List<int>() { 7, 12, -33 };
+            FuncE(e);
+
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+            Console.WriteLine("c:" + String.Join(",", c));
+            Console.WriteLine("d:" + String.Join(",", d));
+            Console.WriteLine("e:" + String.Join(",", e));
+            
+        }
+
+        static void FuncA(int x)
+        {
+            x++;
+        }
+        static int FuncB(int x)
+        {
+            x++;
+            return x;
+        }
+        static void Funcc(List<int> x)
+        {
+            x.Add(49);
+        }
+        static void FuncD(List<int> x)
+        {
+            var temp = new List<int>();
+            temp = x;
+            temp.Add(5);
+        }
+        static void FuncE(List<int> x)
+        {
+            var temp = new List<int>();
+            for (int i = 0; i < x.Count; i++)
+            {
+                temp.Add(x[i]);
+
+            }
+            temp.Add(4);
+        }
+
+    }
+}
